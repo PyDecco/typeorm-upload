1687bec04df8d269b8dff9929d6b0231d7a4c356
André Urpia <dreurpia@gmail.com>
2020-06-06 21:44:08 -0300

    Banco de dados e upload no Node

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+
+import {getCustomRepository} from "typeorm";
+
+import Transaction from "../models/Transaction";
+
+import Transactionsrepository from "../repositories/TransactionsRepository";
 
 class DeleteTransactionService {
   public async execute(): Promise<void> {
-    // TODO
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if(!transaction){
+      throw new AppError('Transaction does not exist');
+    }
+    await transactionsRepository.remove(transaction);
   }
 }
 
